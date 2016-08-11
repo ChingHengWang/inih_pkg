687b40f633d3adb778671626833713546c11b697
ChingHengWang <qoogood1234@gmail.com>
2016-08-11 23:55:39 +0800

    update

--- src/pub_ini.cpp
+++ src/pub_ini.cpp
@@ -2,17 +2,30 @@
 #include "std_msgs/String.h"
 
 #include <sstream>
-#include "../3d_party/inih/cpp/INIReader.h"
+#include "../lib/inih/cpp/INIReader.h"
+#include <iostream>
+using namespace std;
+INIReader reader("/home/zach/catkin_ws/src/inih_pkg/test.ini");
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  INIReader reader("../test.ini");
   ros::Rate loop_rate(10);
   int count = 0;
-  while (ros::ok())
+
+  if (reader.ParseError() < 0) {
+      std::cout << "Can't load 'test.ini'\n";
+     return 1;
+  }
+  cout << "ini content " << endl;
+  cout << "name = "<<reader.Get("my_section","name","UNKNOW")<<endl;
+  cout << "number = "<<reader.Get("my_section","number","UNKNOW")<<endl;
+
+
+  while (!ros::ok())
   {
     std_msgs::String msg;
 
